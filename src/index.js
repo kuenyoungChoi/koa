f1156bfdbd37d6a4ba0a77b38e7a63dcb41fb801
cky <cky@ilark.co.kr>
2022-11-24 16:52:35 +0900

    11.24 - 컨트롤러 CRUD

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa')
 const Router = require('koa-router')
+const bodyParser = require('koa-bodyparser')
 
 const api = require('./api')
 
@@ -8,6 +9,10 @@ const app = new Koa();
 const router = new Router()
 
 router.use('/api', api.routes()); // api라우터 적용
+
+//라우터 적용 전에 bodyparser 적용
+app.use(bodyParser())
+
 
 //app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods())
