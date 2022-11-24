9f27788b1542293635c380a957b3856ae2aea8bd
cky <cky@ilark.co.kr>
2022-11-24 15:40:51 +0900

    11.24 - route설정

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+
+const api = require('./api')
+
+
+const app = new Koa();
+const router = new Router()
+
+router.use('/api', api.routes()); // api라우터 적용
+
+//app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods())
+
+
+
+
+app.listen(4000, () => {
+  console.log('Listening to port 4000')
+})
